4a79d7ab08ddbed8ce1ee324ef25b451d9d1d693
jokj624 <jokj624@gmail.com>
2022-11-20 16:08:52 +0900

    Fix: custom error data 타입 변경

--- src/exceptions/custom.exception.ts
+++ src/exceptions/custom.exception.ts
@@ -2,11 +2,11 @@ import { HttpException } from "@nestjs/common";
 
 export default class CustomException extends HttpException {
   public statusCode: number = 0;
-  public message: string = '';
   public success: boolean = false;
-  public data: any;
+  public message: string = '';
+  public data: unknown;
 
-  constructor(status: number, success: boolean, message?: string, data?: any) {
+  constructor(status: number, success: boolean, message: string, data?: unknown) {
     super(message, status);
     this.statusCode = status;
     this.success = success;
