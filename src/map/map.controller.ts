172c0bfd637567d78b26511cd150176184c29de9
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-11-23 16:25:46 +0900

    Feat: getMapById api 초안

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,7 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { MapService } from './map.service';
+import { MapResponseDto } from './MapResponseDto';
 
 @Controller('map')
 export class MapController {
   constructor(private readonly mapService: MapService) {}
+
+  @Get('/maps/:id?')
+  // async getMapById(
+  //   @Param() param: { id: number },
+  //   @Query() query: { lat: string; lang: string },
+  // ): Promise<string> {
+  //   return 'hello world';
+  // }
+  async getMapById(
+    @Param('id') id: number,
+    @Query('lat') lat: number,
+    @Query('lang') lang: number,
+  ): Promise<MapResponseDto> {
+    return this.mapService.getMapById(id, lat, lang);
+  }
 }
--- src/map/map.service.ts
+++ src/map/map.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { MapResponseDto } from './MapResponseDto';
 
 @Injectable()
-export class MapService {}
+export class MapService {
+  constructor(private prisma: PrismaService) {}
+
+  async getMapById(
+    mapId: number,
+    lat: number,
+    lang: number,
+  ): Promise<MapResponseDto> {
+    try {
+      const map = await this.prisma.map.findUnique({
+        where: {
+          id: mapId,
+        },
+      });
+
+      const data: MapResponseDto = {
+        name: map.areaName,
+        address: map.address,
+        image: map.image,
+        distance: '222m',
+      };
+
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
