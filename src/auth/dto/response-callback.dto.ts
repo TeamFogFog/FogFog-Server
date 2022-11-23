b477f878a7342033cccd0c2454af05b5a4b14388
jokj624 <jokj624@gmail.com>
2022-11-23 23:55:07 +0900

    Feat: success response custom data type 지정

--- src/auth/dto/response-callback.dto.ts
+++ src/auth/dto/response-callback.dto.ts
@@ -1,10 +1,12 @@
 import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { ResponseSuccessDto } from 'src/common/dto/response-success.dto';
 
-export class CallbackResponseDto {
-  @ApiProperty({
-    required: true,
-  })
-  @IsString()
+export class CallbackResponse {
+  @ApiProperty()
   accessToken: string;
+}
+export class ResponseCallbackDto extends ResponseSuccessDto {
+  @ApiProperty()
+  data: CallbackResponse;
 }
---
+++ src/common/types/response-data.type.ts
@@ -0,0 +1,3 @@
+import { CallbackResponse } from 'src/auth/dto/response-callback.dto';
+
+export type ResponseDataType = CallbackResponse;
