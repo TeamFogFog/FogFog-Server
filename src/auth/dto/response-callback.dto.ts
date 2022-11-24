721a21f70b5dd2c7a4775f4ed7cbd226eb8e41e8
jokj624 <jokj624@gmail.com>
2022-11-25 00:21:35 +0900

    Feat: kakao login 관련 dto

--- src/auth/dto/response-callback.dto.ts
+++ src/auth/dto/response-callback.dto.ts
@@ -1,12 +1,11 @@
-import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { ResponseSuccessDto } from 'src/common/dto/response-success.dto';
-
-export class CallbackResponse {
+export class ResponseCallback {
   @ApiProperty()
   accessToken: string;
 }
+
 export class ResponseCallbackDto extends ResponseSuccessDto {
   @ApiProperty()
-  data: CallbackResponse;
+  data: ResponseCallback;
 }
