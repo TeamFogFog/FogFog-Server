a80b021af46bf00dfc3cbc590e6a1338ffa4f5df
jokj624 <jokj624@gmail.com>
2022-11-25 00:22:57 +0900

    Fix: object convert 유틸 함수 반환값 타입 수정

--- src/utils/convertObjectKey.ts
+++ src/utils/convertObjectKey.ts
@@ -2,7 +2,7 @@ export const convertObjectKey = (o: Object, value: unknown) => {
   let convertedKey;
 
   Object.keys(o).forEach((key) => {
-    if (o[key] === value) return (convertedKey = key);
+    if (o[key] === value) return (convertedKey = Number(key));
   });
 
   return convertedKey;
