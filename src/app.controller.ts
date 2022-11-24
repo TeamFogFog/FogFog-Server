5558e68db04234b96187192a9296733485e51bb3
jokj624 <jokj624@gmail.com>
2022-11-24 21:49:35 +0900

    Feat: guard test 용 api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AccessTokenGuard } from './auth/guards';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Get('test')
+  async authenticationTest(@Req() req) {
+    return { data: req.user };
   }
 }
