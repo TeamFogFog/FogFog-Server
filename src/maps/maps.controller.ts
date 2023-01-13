03c7f087bdad7fb9d99b0fa63080a59eed46b314
jokj624 <jokj624@gmail.com>
2023-01-13 21:32:26 +0900

    Feat: 반경 내 흡연구역 전체 조회 API

--- src/maps/maps.controller.ts
+++ src/maps/maps.controller.ts
@@ -1,31 +1,100 @@
-import { Controller, Get, Param, Query, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { MapsService } from './maps.service';
 import {
   ResponseSmokingAreaDto,
   ResponseSmokingAreaData,
   ReadSmokingAreaParam,
+  ReadSmokingAreaQuery,
 } from './dto/read-map.dto';
 import { wrapSuccess } from '../utils/success';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AccessTokenGuard } from 'src/auth/guards';
+import {
+  ReadSmokingAreasQuery,
+  ResponseSmokingAreasData,
+  ResponseSmokingAreasDto,
+} from './dto/read-maps.dto';
 
 @Controller('maps')
 @ApiTags('maps')
 export class MapsController {
   constructor(private readonly mapService: MapsService) {}
 
+  @UseGuards(AccessTokenGuard)
+  @Get('')
+  @ApiOperation({
+    summary: '흡연구역 전체 조회 API',
+    description: '현재 위/경도 기준 반경 2km 이내 흡연구역 조회',
+  })
+  @ApiBearerAuth('accessToken')
+  @ApiQuery({
+    type: Number,
+    name: 'lat',
+    required: true,
+    description: '중심 latitude',
+  })
+  @ApiQuery({
+    type: Number,
+    name: 'long',
+    required: true,
+    description: '중심 longitude',
+  })
+  @ApiOkResponse({ type: ResponseSmokingAreasDto })
+  @ApiBadRequestResponse()
+  @ApiInternalServerErrorResponse()
+  async getMapsByLatAndLong(
+    @Query() { lat, long }: ReadSmokingAreasQuery,
+  ): Promise<ResponseSmokingAreasDto> {
+    const data: ResponseSmokingAreasData =
+      await this.mapService.getMapsByLatAndLong(lat, long);
+
+    return wrapSuccess(HttpStatus.OK, '흡연구역 전체 조회 성공', data);
+  }
+
   @Get(':id')
   @ApiOperation({
     summary: '흡연구역 상세 조회 API',
   })
+  @ApiBearerAuth('accessToken')
+  @ApiQuery({
+    type: Number,
+    name: 'lat',
+    required: true,
+    description: '중심 latitude',
+  })
+  @ApiQuery({
+    type: Number,
+    name: 'long',
+    required: true,
+    description: '중심 longitude',
+  })
+  @ApiOkResponse({ type: ResponseSmokingAreaDto })
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
   async getMapById(
     @Param() { id }: ReadSmokingAreaParam,
-    @Query('lat') lat: number,
-    @Query('lang') lang: number,
+    @Query() { lat, long }: ReadSmokingAreaQuery,
   ): Promise<ResponseSmokingAreaDto> {
     const data: ResponseSmokingAreaData = await this.mapService.getMapById(
       id,
       lat,
-      lang,
+      long,
     );
     return wrapSuccess(HttpStatus.OK, '흡연구역 상세 조회 성공', data);
   }
