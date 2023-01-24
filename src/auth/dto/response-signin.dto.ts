ce50a16814e481b595cacadaa584cae6ae871bf5
jokj624 <jokj624@gmail.com>
2023-01-24 17:03:52 +0900

    Refactor: dto name 통일

--- src/auth/dto/response-signin.dto.ts
+++ src/auth/dto/response-signin.dto.ts
@@ -2,12 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { ResponseSuccessDto } from 'src/common/dto/response-success.dto';
 import { ResponseTokenData } from './response-token.dto';
 
-export class ResponseSignInData extends ResponseTokenData {
+export class ResponseSigninData extends ResponseTokenData {
   @ApiProperty({ description: 'id' })
   id: number;
 }
 
-export class ResponseSignInDto extends ResponseSuccessDto {
+export class ResponseSigninDto extends ResponseSuccessDto {
   @ApiProperty()
-  data: ResponseSignInData;
+  data: ResponseSigninData;
 }
