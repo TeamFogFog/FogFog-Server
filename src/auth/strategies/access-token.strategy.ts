9f54721036e84604dd5d8ac847001198219ad44a
jokj624 <jokj624@gmail.com>
2023-01-24 16:59:22 +0900

    Fix: guard 내에서 user 존재 검증

--- src/auth/strategies/access-token.strategy.ts
+++ src/auth/strategies/access-token.strategy.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from 'src/common/interfaces/jwt-payload.interface';
+import { RESPONSE_MESSAGE } from 'src/common/objects';
+import CustomException from 'src/exceptions/custom.exception';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
+  constructor(config: ConfigService, private prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,7 +17,21 @@ export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: JwtPayload) {
+  async validate(payload: JwtPayload): Promise<JwtPayload> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.id,
+        isDeleted: false,
+      },
+    });
+
+    if (!user) {
+      throw new CustomException(
+        HttpStatus.NOT_FOUND,
+        RESPONSE_MESSAGE.NOT_FOUND,
+      );
+    }
+
     return {
       id: payload.id,
       uniqueId: payload.uniqueId,
