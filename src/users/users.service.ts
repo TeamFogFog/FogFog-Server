3fdec57d2b31cbf2ef4558d64e817289c8f673bf
seohyun-106 <changseohyun@sookmyung.ac.kr>
2023-02-02 01:43:48 +0900

    Feat: 선호지도 수정 API Service 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,6 +5,7 @@ import CustomException from 'src/exceptions/custom.exception';
 import { PrismaService } from 'src/prisma.service';
 import { ResponseNicknameData } from './dto/response-nickname.dto';
 import { UpdateNicknameDto } from './dto/update-nickname.dto';
+import { UpdatePreferredMapDto } from './dto/update-preferredMap.dto';
 
 @Injectable()
 export class UsersService {
@@ -158,6 +159,47 @@ export class UsersService {
       return {
         nickname: updatedUser.nickname,
       };
+    } catch (error) {
+      this.logger.error({ error });
+      throw new CustomException(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        RESPONSE_MESSAGE.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async updatePreferredMapByUserId(
+    userId: number,
+    id: number,
+    updatePreferredMapDto: UpdatePreferredMapDto,
+  ): Promise<void> {
+    if (userId !== id) {
+      throw new CustomException(
+        HttpStatus.FORBIDDEN,
+        RESPONSE_MESSAGE.FORBIDDEN,
+      );
+    }
+
+    if (updatePreferredMapDto.preferredMap > 2) {
+      throw new CustomException(
+        HttpStatus.BAD_REQUEST,
+        RESPONSE_MESSAGE.BAD_REQUEST,
+      );
+    }
+
+    try {
+      const updatedUser = await this.prisma.user.update({
+        where: { id, isDeleted: false },
+        data: {
+          preferredMap: updatePreferredMapDto.preferredMap,
+        },
+      });
+      if (!updatedUser) {
+        throw new CustomException(
+          HttpStatus.NOT_FOUND,
+          RESPONSE_MESSAGE.NOT_FOUND,
+        );
+      }
     } catch (error) {
       this.logger.error({ error });
       throw new CustomException(
